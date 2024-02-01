e08a08d6b1965257f98eed755677c655e0cba2e5
Voahary Mampionona <hei.zoarisoa@gmail.com>
2024-02-01 18:16:53 +0300

    feat: add bigBody in App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import './body.css';
+import './bigBody.css'
 import Docs from './components/docs';
 import Body from './components/body'
+import BigBodys from './components/bigBody';
 
 function App() {
   return (    
     <div className="HeadBody">
            <Docs/> 
            <Body/>    
+           <BigBodys/>
     </div>
   );
 }
