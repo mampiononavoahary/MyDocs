a4951bd96093bdd48e0ed9a2ee1f9a92d1932d98
Voahary Mampionona <hei.zoarisoa@gmail.com>
2024-02-01 18:15:12 +0300

    feat: add components bigBody

---
+++ src/components/bigBody.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function bigBodys() {
+    return(
+        <div className="SecondBody">
+            <div className="AllDocs">
+                    <div className="carddoc"></div>
+                    <div className="carddoc"></div>
+                    <div className="carddoc"></div>
+                    <div className="carddoc"></div>
+                    <div className="carddoc"></div>
+            </div>
+        </div>
+    );
+}
